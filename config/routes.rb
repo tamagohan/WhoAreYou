4a46fdb0c8220cced1adfe01692ab4c5eed8aafd
masaki_mac <fantastipo23@gmai.com>
2012-01-01 16:38:54 +0900

    change top of site to account_sessions/new

--- config/routes.rb
+++ config/routes.rb
@@ -90,6 +90,7 @@ end
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => 'account_sessions#new'
 
   # See how all your routes lay out with "rake routes"
 
